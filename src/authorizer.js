17f84dfe55e0c16f0c9c6a3ef7850aa3227ce28e
Mark Tse <mark.tse@neverendingqs.com>
2019-07-01 13:39:18 -0400

    chore - adjusting comment in authorizer.

--- src/authorizer.js
+++ src/authorizer.js
@@ -20,9 +20,10 @@ exports.handler = async function(event, context) {
   console.log("EVENT: \n" + JSON.stringify(event, null, 2));
 
   const { headers, methodArn } = event;
+
+  // This is for demo purposes only.
+  // This check is probably not valuable in production.
   if(headers['X-Forwarded-Proto'] === 'https') {
-    // This is for demo purposes only. This check is probably not valuable in
-    // production.
     return createAuthorizedResponse(methodArn);
   } else {
     return createUnauthorizedResponse();
